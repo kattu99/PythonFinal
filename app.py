c1792583a595da9511c1ae81a6981e62e6958bc2
kattu99 <kataria.rahul172@gmail.com>
2020-05-15 01:04:03 -0400

    Finished

--- app.py
+++ app.py
@@ -14,7 +14,21 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
+#static route
+@app.route("/books")
+def books():
+    return render_template('books.html')
+
+#static route
+@app.route("/classes")
+def classes():
+    return render_template('classes.html')
+
+@app.route("/photo")
+def photo():
+    return render_template('Photo-Applying.jpg')
 
 #start the server
 if __name__ == "__main__":
